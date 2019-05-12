fcf9b6d6c0fa9b9befa8d2dc9a6e0fd967d850fe
furkankaratas <furkan.karatas02@gmail.com>
2019-05-13 02:00:26 +0300

    test to get and show image

--- app/src/main/java/com/karatas/furkan/flickrclient/MainActivity.java
+++ app/src/main/java/com/karatas/furkan/flickrclient/MainActivity.java
@@ -2,12 +2,56 @@ package com.karatas.furkan.flickrclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.flickr.com/services/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        FlickrApi flickrApi = retrofit.create(FlickrApi.class);
+
+        Call<FlickrResponse> call = flickrApi.getRecentPhotos();
+
+        call.enqueue(new Callback<FlickrResponse>() {
+
+            @Override
+            public void onResponse(Call<FlickrResponse> call, Response<FlickrResponse> response) {
+                FlickrResponse flickrResponse = response.body();
+                String url = flickrResponse.getPhotos().getPhotoList().get(0).generateUrl();
+                Glide
+                        .with(getApplicationContext())
+                        .load(url)
+                        //.thumbnail(0.1f)
+                        .into(imageView);
+            }
+
+            @Override
+            public void onFailure(Call<FlickrResponse> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getMessage());
+            }
+        });
     }
 }
