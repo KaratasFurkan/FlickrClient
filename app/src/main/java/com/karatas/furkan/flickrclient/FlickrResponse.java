fc9dfe2bd7a2c8598a3563acaf7406f4f85db9d8
furkankaratas <furkan.karatas02@gmail.com>
2019-05-13 01:57:24 +0300

    created a class for flickr api response

---
+++ app/src/main/java/com/karatas/furkan/flickrclient/FlickrResponse.java
@@ -0,0 +1,45 @@
+package com.karatas.furkan.flickrclient;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class FlickrResponse {
+
+    private Photos photos;
+    private String stat;
+
+    public Photos getPhotos() {
+        return photos;
+    }
+
+    public class Photos{
+        private int page;
+        private int pages;
+        private int perpage;
+        private int total;
+        @SerializedName("photo")
+        private List<Photo> photoList = null;
+
+        public List<Photo> getPhotoList() {
+            return photoList;
+        }
+    }
+
+    public class Photo{
+        private String id;
+        private String secret;
+        private String server;
+        private int farm;
+        private String title;
+        private String owner;
+        private int ispublic;
+        private int isfriend;
+        private int isfamily;
+
+        public String generateUrl(){
+            return "https://farm" + farm + ".staticflickr.com/" + server + "/" +
+                    id + "_" + secret + ".jpg";
+        }
+    }
+}
