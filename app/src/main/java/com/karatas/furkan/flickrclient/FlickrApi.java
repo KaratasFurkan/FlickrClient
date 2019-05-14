5865bec6995da83a621f78922803f4f0cb37b7d9
furkankaratas <furkan.karatas02@gmail.com>
2019-05-14 05:18:05 +0300

    changed link

--- app/src/main/java/com/karatas/furkan/flickrclient/FlickrApi.java
+++ app/src/main/java/com/karatas/furkan/flickrclient/FlickrApi.java
@@ -5,6 +5,6 @@ import retrofit2.http.GET;
 
 public interface FlickrApi {
 
-    @GET("rest/?method=flickr.photos.getRecent&api_key=6d0a61a8a0d0105537fd86293e4d905c&format=json&nojsoncallback=1&auth_token=72157678209036077-ce55c986840f8a3b&api_sig=1bdcbc286b36e517f4dae43603029452")
+    @GET("rest/?method=flickr.photos.getRecent&api_key=15a9793153b71f937ee835c596dfbd7a&format=json&nojsoncallback=1&auth_token=72157678235273657-c49b34bdc08e0558&api_sig=a69d0ee682afabcdef4ef6e8884df74b")
     Call<FlickrResponse> getRecentPhotos();
 }
