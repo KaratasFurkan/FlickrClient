b6fdce62091d3a82ca748d50c4cfc032b48a60c7
furkankaratas <furkan.karatas02@gmail.com>
2019-05-15 04:04:39 +0300

    removed temporary url, added query map

--- app/src/main/java/com/karatas/furkan/flickrclient/FlickrApi.java
+++ app/src/main/java/com/karatas/furkan/flickrclient/FlickrApi.java
@@ -1,10 +1,12 @@
 package com.karatas.furkan.flickrclient;
 
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
 
 public interface FlickrApi {
-
-    @GET("rest/?method=flickr.photos.getRecent&api_key=15a9793153b71f937ee835c596dfbd7a&format=json&nojsoncallback=1&auth_token=72157678235273657-c49b34bdc08e0558&api_sig=a69d0ee682afabcdef4ef6e8884df74b")
-    Call<FlickrResponse> getRecentPhotos();
+    @GET("rest/")
+    Call<FlickrResponse> getPhotos(@QueryMap Map<String, String> parameters);
 }
