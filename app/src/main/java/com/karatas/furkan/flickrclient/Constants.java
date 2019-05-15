5e904894217b9673503ac72118a1831eb7f432e4
furkankaratas <furkan.karatas02@gmail.com>
2019-05-15 04:03:12 +0300

    added Constants class for api methods, arguments etc.

---
+++ app/src/main/java/com/karatas/furkan/flickrclient/Constants.java
@@ -0,0 +1,11 @@
+package com.karatas.furkan.flickrclient;
+
+public class Constants {
+    public static final String BASE_URL = "https://api.flickr.com/services/";
+    public static final String API_KEY = "7503f14905e643ff4531ab786590d171";
+    public static final String METHOD_RECENT = "flickr.photos.getRecent";
+    public static final String METHOD_SEARCH = "flickr.photos.search";
+    public static final String FORMAT = "json";
+    public static final String PER_PAGE = "500";
+    public static final String NOJSONCALLBACK = "1";
+}
